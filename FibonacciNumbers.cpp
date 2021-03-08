244a589726d6763365ff54210475635da2912235
aarnavg17 <67118298+aarnavg17@users.noreply.github.com>
2021-03-08 17:39:30 +0800

    FibonacciNumbers

--- FibonacciNumbers.cpp
+++ FibonacciNumbers.cpp
@@ -1,17 +1,24 @@
-//Naive Algorithm
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int fib(int n) {
-    if (n <= 1){
-        return(n);
+    if (n <= 1) {
+        return (n);
     }
     else {
-        return(fib(n - 1) + fib(n - 2));
+        long long int A = 0, B = 1, C;
+        C = A + B;
+        for (int i = 0; i < n - 2; i++) {
+            A = B;
+            B = C;
+            C = A + B;
+        }
+        return (C);
     }
 }
 
 int main() {
-    int n = 20;
+    long long int n;
+    cin >> n;
     cout << fib(n);
 }
